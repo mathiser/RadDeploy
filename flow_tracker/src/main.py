91e24617e041f0c62e00fac812520b08842e8c85
Mathis Ersted Rasmussen <mathiser@gmail.com>
2024-01-26 12:02:08 +0100

    Implements FlowTracker as DB for grafana dashboard. Refactored Janitor to only take care of files (#18)
    Co-authored-by: Mathis Rasmussen <mathis.rasmussen@rm.dk>

---
+++ flow_tracker/src/main.py
@@ -0,0 +1,70 @@
+import os
+import signal
+
+from DicomFlowLib.conf import load_configs
+from DicomFlowLib.data_structures.contexts import SubModel
+from DicomFlowLib.fs import FileStorage
+from DicomFlowLib.log import CollectiveLogger
+from DicomFlowLib.mq import MQSub
+from flow_tracker import FlowTracker
+
+
+class Main:
+    def __init__(self, config):
+        signal.signal(signal.SIGTERM, self.stop)
+        self.running = False
+        self.logger = CollectiveLogger(name=config["LOG_NAME"],
+                                       log_level=int(config["LOG_LEVEL"]),
+                                       log_format=config["LOG_FORMAT"],
+                                       log_dir=config["LOG_DIR"],
+                                       rabbit_hostname=config["RABBIT_HOSTNAME"],
+                                       rabbit_port=int(config["RABBIT_PORT"]),
+                                       rabbit_password=config["RABBIT_PASSWORD"],
+                                       rabbit_username=config["RABBIT_USERNAME"])
+
+        self.ft = FlowTracker(logger=self.logger,
+                              database_path=config["DATABASE_PATH"],
+                              dashboard_rules=config["DASHBOARD_RULES"])
+
+        self.mq = MQSub(logger=self.logger,
+                        work_function=self.ft.mq_entrypoint,
+                        rabbit_hostname=config["RABBIT_HOSTNAME"],
+                        rabbit_port=int(config["RABBIT_PORT"]),
+                        sub_models=[SubModel(**d) for d in config["SUB_MODELS"]],
+                        pub_models=[],
+                        sub_prefetch_value=int(config["SUB_PREFETCH_COUNT"]),
+                        sub_queue_kwargs=config["SUB_QUEUE_KWARGS"])
+
+    def start(self):
+        self.logger.debug("Starting Janitor", finished=False)
+        self.running = True
+        self.logger.start()
+        self.mq.start()
+        self.logger.debug("Starting Janitor", finished=True)
+
+        while self.running:
+            try:
+                self.mq.join(timeout=5)
+                if self.mq.is_alive():
+                    pass
+                else:
+                    self.stop()
+            except KeyboardInterrupt:
+                self.stop()
+
+    def stop(self):
+        self.logger.debug("Stopping Janitor", finished=False)
+        self.running = False
+        self.ft.stop()
+        self.logger.stop()
+
+        self.ft.join()
+        self.logger.join()
+        self.logger.debug("Stopping Janitor", finished=True)
+
+
+if __name__ == "__main__":
+    config = load_configs(os.environ["CONF_DIR"])
+
+    m = Main(config=config)
+    m.start()
