b09edd87d062733e09da99bca6ecde592812efb8
Mathis Rasmussen <mathis.rasmussen@rm.dk>
2024-04-11 14:06:37 +0200

    fingerprinter tests implemented

--- fingerprinter/tests/test_fp_utils.py
+++ fingerprinter/tests/test_fp_utils.py
@@ -10,6 +10,7 @@ from fingerprinter.src.fingerprinter.fp_utils import parse_fingerprints, generat
 def flow_dir():
     return "tests/test_data/flows/"
 
+
 @pytest.fixture
 def scp_tar():
     with open("tests/test_data/scans/scp.tar", "rb") as r:
@@ -21,26 +22,26 @@ def test_parse_fingerprints(flow_dir):
     for flow in flows:
         isinstance(flow, Flow)
 
+
 def test_generate_df_from_tar(scp_tar):
     df = generate_df_from_tar(scp_tar)
-    assert "dcm_path" in df.columns
-    assert df["dcm_path"].tolist() == ['ct.dcm', 'mr.dcm']
+    assert "path" in df.columns
+    assert df["path"].tolist() == ['ct.dcm', 'mr.dcm']
+
 
 def test_generate_flow_specific_tar(scp_tar):
     df = generate_df_from_tar(scp_tar)
     sliced_tar = generate_flow_specific_tar(tar_file=scp_tar, sliced_df=df[df["Modality"] == "MR"])
-    with open("t.tar", "wb") as f:
-        f.write(sliced_tar.read())
     sliced_df = generate_df_from_tar(sliced_tar)
-    assert sliced_df["path"].tolist() == ["MR_1.3.46.670589.11.71891.5.0.8000.2022062210324747248_1.3.46.670589.11.71891.5.0.8000.2022062210385801250.dcm"]
+    assert sliced_df["path"].tolist() == [
+        "MR_1.3.46.670589.11.71891.5.0.8000.2022062210324747248_1.3.46.670589.11.71891.5.0.8000.2022062210385801250.dcm"]
 
     sliced_tar = generate_flow_specific_tar(tar_file=scp_tar, sliced_df=df[df["Modality"] == ["CT", "MR"]])
-    with open("t.tar", "wb") as f:
-        f.write(sliced_tar.read())
     sliced_df = generate_df_from_tar(sliced_tar)
     assert sliced_df["path"].tolist() == [
         'CT_1.3.6.1.4.1.40744.29.33371661027192187491509798061184654147_1.3.6.1.4.1.40744.29.179461095576983824275091091253440398486.dcm',
         "MR_1.3.46.670589.11.71891.5.0.8000.2022062210324747248_1.3.46.670589.11.71891.5.0.8000.2022062210385801250.dcm"]
+
 
 def test_slice_dataframe_to_triggers(scp_tar):
     ct_trigger = [
@@ -52,5 +53,7 @@ def test_slice_dataframe_to_triggers(scp_tar):
     sliced_tar = generate_flow_specific_tar(tar_file=scp_tar, sliced_df=sliced_df)
     sliced_tar_df = generate_df_from_tar(sliced_tar)
     assert set(sliced_tar_df["SeriesInstanceUID"].tolist()) == set(sliced_df["SeriesInstanceUID"].tolist())
+
+
 if __name__ == '__main__':
     pytest.main()
